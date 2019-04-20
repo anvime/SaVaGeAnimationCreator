a03434c25020dd50c94db8b52b789426309c5784
ost96 <37448385+ost96@users.noreply.github.com>
2019-04-20 23:33:33 +0200

    Basic react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Button from "@material-ui/core/Button/Button";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
